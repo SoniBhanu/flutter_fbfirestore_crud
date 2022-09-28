d03a189a110af8a1a8d160aa102fc77f811b20b6
SoniKraken <bhanusoni@jklu.edu.in>
2022-09-28 16:19:48 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,29 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_fbfirestore_crud/pages/home_page.dart';
+import 'package:flutter_fbfirestore_crud/pages/login.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot) {
-        // CHeck for Errors
-        if (snapshot.hasError) {
-          print("Something went Wrong");
-        }
-        // once Completed, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return MaterialApp(
-            title: 'Flutter Firestore CRUD',
-            theme: ThemeData(
-              primarySwatch: Colors.blue,
-            ),
-            debugShowCheckedModeBanner: false,
-            home: HomePage(),
-          );
-        }
-        return CircularProgressIndicator();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Firebase Firestore',
+      initialRoute: "/login",
+      routes: {
+        "/":(context) => HomePage(),
+        "/login":(context) => Login(),
       },
     );
   }
 }
+
